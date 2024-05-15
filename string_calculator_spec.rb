86e602d960fcfc0acccee7ab3d6d4507daf38c02
Rahul Kashyap <rahul.kashyap@yabx.co>
2024-05-15 10:07:02 +0530

    initial Commit: handles empty string with rspec and ruby StringCalculator class

---
+++ string_calculator_spec.rb
@@ -0,0 +1,11 @@
+require_relative 'string_calculator'
+RSpec.describe StringCalculator do
+  describe '#add' do
+    context 'when given an empty string' do
+      it 'returns 0' do
+        calculator = StringCalculator.new
+        expect(calculator.add('')).to eq(0)
+      end
+    endha
+  end
+end
