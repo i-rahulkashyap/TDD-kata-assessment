ac461724584b9053b58f441a085228f380fb3445
Rahul Kashyap <rahul.kashyap@yabx.co>
2024-05-15 10:10:06 +0530

    Handles: one or two number in input string

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -7,5 +7,20 @@ RSpec.describe StringCalculator do
         expect(calculator.add('')).to eq(0)
       end
     end
+
+    context 'when given one number' do
+        it 'returns the same number' do
+          calculator = StringCalculator.new
+          expect(calculator.add('1')).to eq(1)
+        end
+    end
+
+    context 'when given two numbers' do
+        it 'returns the sum of the numbers' do
+          calculator = StringCalculator.new
+          expect(calculator.add('1,2')).to eq(3)
+        end
+    end
+
   end
 end
